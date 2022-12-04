130fb60ec7cef11abc6bbf81d45c78624d259d5a
Minseok Kim <comko123@naver.com>
2022-12-05 00:15:00 +0900

    .

--- src/api.ts
+++ src/api.ts
@@ -2,18 +2,14 @@ import axios from "axios"
 export interface setStateType{
 [key:string]:string
 }
-
 export const favorite:setStateType[] = []
-
 const pushing = (parameter:setStateType[]) => {
     for(let i=0; i<6; i++){
-        const favor = parameter[i]
-        favorite.push(favor)}}
-
+    const favor = parameter[i]
+    favorite.push(favor)}}
 export const productAxios = async(storageBox:(item:setStateType[])=>void,text:string) => {
 const {data:{items}} = await axios("/v1/search/shop.json",
 {params:{query : text ,display:30},headers:{
   "X-Naver-Client-Id": "DgxIVVws_iVRpSppgOIW",
   "X-Naver-Client-Secret":"KcFNGsYQ03"}})
-  pushing(items)
-  return (storageBox(items))}
+  pushing(items);return(storageBox(items))}
