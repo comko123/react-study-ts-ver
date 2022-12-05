d13b717bc8c1d4e4707bcc7c0f1f39688e789878
Minseok Kim <comko123@naver.com>
2022-12-06 03:19:42 +0900

    .

--- src/Rout.tsx
+++ src/Rout.tsx
@@ -1,13 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-import MainPage from "./MainPage";
+import { Routes, Route } from "react-router-dom"
+import MainPage from "./MainPage"
 const Rout = () => {
-return<>
- <Routes>
- <Route path="/" element={<MainPage/>}/>
-
- </Routes>
-</>
-
-
-}
+return<><Routes>
+<Route path="/" element={<MainPage/>}/>
+</Routes></>}
 export default Rout
