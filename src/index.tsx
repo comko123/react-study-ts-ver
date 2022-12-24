6de8cb15602d607949fadce77ea89e1c49a38eba
Minseok Kim <comko123@naver.com>
2022-12-25 00:18:02 +0900

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import App from './App';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient ,QueryClientProvider} from 'react-query';
-const queryClient = new QueryClient();
+import App from './App'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient ,QueryClientProvider} from 'react-query'
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
   <BrowserRouter>
   <QueryClientProvider client={queryClient}>
     <App />
     </QueryClientProvider>
     </BrowserRouter>
-);
+)
